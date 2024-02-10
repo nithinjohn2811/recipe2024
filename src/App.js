316749eba64f4fd46f799afd16a6129b275490eb
Nithin John Cherian <Nithinjohn2811"gmail.com>
2024-02-10 17:18:05 +0000

    routes created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Recipe from "./pages/recipe/Recipe";
+import Search from "./pages/search/Search";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <BrowserRouter>
+        <Switch>
+          <Route exact path = "/"> <Home /></Route>
+          <Route exact path = "/create"> <Create /></Route>
+          <Route exact path = "/recipe/:id"><Recipe /></Route>
+          <Route exact path = "/search"><Search /></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
